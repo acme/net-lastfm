46bf1df2ba5c2a73131167b2fe982ed70e631cd2
Leon Brocard <acme@astray.com>
2008-12-14 15:33:56 +0000

    and release 0.33

--- lib/Net/LastFM.pm
+++ lib/Net/LastFM.pm
@@ -5,7 +5,7 @@ use Digest::MD5 qw(md5_hex);
 use JSON::XS::VersionOneAndTwo;
 use LWP::UserAgent;
 use URI::QueryParam;
-our $VERSION = '0.32';
+our $VERSION = '0.33';
 
 has 'api_key' => (
     is       => 'rw',
